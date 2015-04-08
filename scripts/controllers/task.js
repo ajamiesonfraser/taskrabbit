3f34b84c6e7856143c84af44bcb4d8e474489721
A.J. Fraser <ajamiesonfraser@gmail.com>
2015-04-08 15:40:00 -0300

    added toaster notifications

--- scripts/controllers/task.js
+++ scripts/controllers/task.js
@@ -1,10 +1,10 @@
 'use strict';
 
-app.controller('TaskController', function($scope, FURL, $firebase, $location, $routeParams) {
+app.controller('TaskController', function($scope, FURL, $firebase, $location, $routeParams, toaster) {
 	var ref = new Firebase(FURL);
 	var fbTasks = $firebase(ref.child('tasks')).$asArray();
 	var taskId = $routeParams.taskId;
-	
+
 	if(taskId){
 		$scope.selectedTask = getTask(taskId);
 	}
@@ -15,12 +15,14 @@ app.controller('TaskController', function($scope, FURL, $firebase, $location, $r
 
 	$scope.updateTask = function(task) {
 		$scope.selectedTask.$save(task);
+		toaster.pop('success', 'Task is updated.');
 		$location.path('/browse');
 	}
 	$scope.tasks = fbTasks;
 
 	$scope.postTask = function(task) {
 		fbTasks.$add(task);
+		toaster.pop('success', 'Task is updated.');
 		$location.path('/browse');
 	}
 });
